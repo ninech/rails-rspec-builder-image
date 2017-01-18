18fc22348b6633addf557e0ebf3f895fc4ffa4ce
Philippe Hässig <phil@nine.ch>
2017-01-18 15:43:38 +0100

    First build of a rails app is running

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
 FROM docker-registry-default.nine.ch/ninech/ruby:xenial
 
+# Install some commonly used gems to make builds quicker
+RUN gem install --no-ri --no-rdoc nokogiri -v 1.6.8.1
+RUN gem install --no-ri --no-rdoc pg -v 0.19.0
+RUN gem install --no-ri --no-rdoc spring -v 2.0.0
+RUN gem install --no-ri --no-rdoc rails -v 5.0.0.1
+RUN gem install --no-ri --no-rdoc ffi -v 1.9.17
+
+RUN apt-get update \
+    && apt-get install -y pwgen postgresql-client \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
 COPY build.sh /tmp/
 
 CMD ["/tmp/build.sh"]
--- build.sh
+++ build.sh
@@ -1,14 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-echo "Sleeping for one hour"
-sleep 1h
+function cleanup {
+  # cannot use rake db:drop because environment entry is gone
+  # https://github.com/DatabaseCleaner/database_cleaner/issues/445
+  export PGPASSWORD=$POSTGRES_PASSWORD
+  dropdb -h $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_DATABASE
+}
 
-if [ "$FAIL" ]; then
-  echo "Failing..."
-  /bin/false
-else
-  echo "Build successful..."
-  /bin/true
-fi
+export RAILS_ENV=test
+export POSTGRES_DATABASE=$(pwgen -1 25)
+
+env
+
+echo "📦  Unpacking stuff from STDIN"
+tar zxf -
+
+echo "🚚  Start bundle install"
+bundle install
+
+echo "💻  Setup database"
+bin/rails db:create
+trap cleanup EXIT
+bin/rails db:schema:load
+
+echo "🛠  Running the tests"
+xvfb-run -a bundle exec rspec --format documentation
